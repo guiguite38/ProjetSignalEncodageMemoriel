7a169b09c591d064665f5d8fb4e61b865c23d3ff
Guillaume <ggrosse@ensc.fr>
2019-12-03 17:07:35 +0100

    TP2 - Determination du profil global

--- RegulariteSignal.m
+++ RegulariteSignal.m
@@ -1,7 +1,7 @@
 clear all
 close all
 
-M=1000;
+M=1010;
 
 %Régularité = Quantification du chaos
 %On va implémenter des méthodes pour estimer la régularité ie alpha € [0.5,1.5]
@@ -9,9 +9,12 @@ M=1000;
 % alpha < 1.5 => mémoire courte
 % alpha > 1.5 => mémoire longue
 
-%%Processus
-% on intègre le signal : y => Y
 
+%---------------------------------
+%===== INTEGRATION DE y EN Y =====
+%---------------------------------
+
+% bruit blanc gaussien
 moy = 5;
 var = sqrt(2);
 
@@ -24,22 +27,72 @@ for i=2:M
     Y(i) = Y(i-1) + yc(i);
 end
 
-%Intégrale
+%tracé de l'intégrale Y de y
 plot(Y)
 
-nbFenetres = 10;
-% params contient les coefs d'ordre 2, 1 et 0 du polynôme de degré 2
-% approchant la courbe sur la fenêtre i
-params = zeros(3,nbFenetres-1);
+tabN = [11,13,17,21,27,35,47,59,77,101];
+Fn = zeros(1,10);
+count = 0;
 
-for i=1:9
-    segment = Y(1,i*M/nbFenetres:(i+1)*M/nbFenetres);
-    params(:,i) = polyfit(i*M/nbFenetres:(i+1)*M/nbFenetres,segment,2); 
+for N = tabN
+    count=count+1;    
+    
+    %---------------------------------------------------------
+    %===== DETERMINATION DES TENDANCES DE CHAQUE FENETRE =====
+    %---------------------------------------------------------
+    
+    L = floor(M/N);
+    % params contient les coefs d'ordre 2, 1 et 0 du polynôme de degré 2
+    % approchant la courbe sur la fenêtre i
+    params = zeros(3,L);
+
+    for i=1:L
+        segment = Y(1,(i-1)*N+1:i*N);
+        params(:,i) = polyfit((i-1)*N+1:i*N,segment,2);
+    end
+
+    estimateY = zeros(1,L*N);
+    x=0;
+    for i=1:L
+        for j=1:N
+            x=x+1;                        
+            estimateY(x)=params(1,i)*x*x + params(2,i)*x + params(3,i);
+        end
+    end
+    
+    %-----------------------------------------------------
+    %===== CALCUL DE LA SOMME DES CARRES DES RESIDUS =====
+    %-----------------------------------------------------   
+        
+    R = Y(1:L*N)-estimateY;
+
+    figure,
+    subplot(2,1,1)
+    plot(estimateY);
+    title('tracé de estimateY')
+    subplot(2,1,2)
+    plot(Y)
+    title('tracé de Y')
+
+    %-----------------------------------------------------------
+    %===== CALCUL DE LA RACINE DE LA PUISSANCE DES RESIDUS =====
+    %-----------------------------------------------------------
+    
+    Fn(count)=1/(L*N)*sqrt(sum(R.^2));
 end
 
-% fenêtrage => on a une tendance par fenêtre
-% calcul résidus # résidu d'une fenêtre = Somme(|signal-tendance|)
 
-%on trace log(F(n))
-% N le nombre de fenêtres
-% F(N) l'amplitude carrée normalisée #on enlève la moyenne
+%%
+%--------------------------------------------------------------------
+%===== ESTIMATION DE ALPHA AVEC REGRESSION LINEAIRE SUR log(Fn) =====
+%--------------------------------------------------------------------
+
+figure,
+subplot(2,1,1)
+plot(tabN,Fn);
+subplot(2,1,2)
+plot(log(tabN),log(Fn),'o')
+title('log(Fn) en fonction de log(tabN)');
+
+B = zeros(2,1);
+B(:,1) = polyfit(log(tabN),log(Fn),1);
